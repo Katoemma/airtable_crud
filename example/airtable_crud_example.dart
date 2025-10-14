f1c6108ed0c6e58fe385afabdfcdc6f7f8e12928
Jeff Miller <jmiller@pwiworks.com>
2025-10-14 11:26:24 -0400

    fix example code

--- example/airtable_crud_example.dart
+++ example/airtable_crud_example.dart
@@ -1,6 +1,5 @@
+import 'package:airtable_crud/airtable_plugin.dart';
 import 'package:flutter/material.dart';
-import 'package:airtable_crud/airtable_crud.dart';
-import 'package:airtable_crud/models/airtable_record.dart';
 
 void main() {
   runApp(AirtableExampleApp());
@@ -93,7 +92,7 @@ class _AirtableHomePageState extends State<AirtableHomePage> {
     ];
 
     try {
-      await airtableCrud.bulkCreateRecords(tableName, dataList);
+      await airtableCrud.createBulkRecords(tableName, dataList);
       await fetchRecords();
     } catch (e) {
       print('Error bulk creating records: $e');
